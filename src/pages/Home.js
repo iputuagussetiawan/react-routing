b88e06d5845b9f7500e80d0e2936c7078b83bad9
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-03-25 08:49:49 +0800

    Navigating between Pages with Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+     <h1>Home</h1>
+     <p>Hallow From Home</p>
+     <Link to="/products">Go To Product</Link>
+    </>
   )
 }
 
